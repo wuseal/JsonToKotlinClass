c9299541083242fc4dc33b4fe5a97045dfae2060
seal <sealwu@iresearch.com.cn>
2018-04-18 17:16:48 +0800

    Release 1.7.3

---
+++ src/wu/seal/jsontokotlin/utils/ClassCodeFilter.kt
@@ -0,0 +1,26 @@
+package wu.seal.jsontokotlin.utils
+
+import wu.seal.jsontokotlin.ConfigManager
+
+/**
+ * Class Code Filter
+ * Created by Seal.Wu on 2018/4/18.
+ */
+object ClassCodeFilter {
+
+    /**
+     * when not in `innerClassModel` and the class spit with `\n\n` then remove the duplicate class
+     */
+    fun removeDuplicateClassCode(generateClassesString: String): String {
+
+        if (ConfigManager.isInnerClassModel.not()) {
+
+            val set = mutableSetOf<String>()
+            set.addAll(generateClassesString.split("\n\n"))
+            return set.joinToString("\n\n")
+
+        } else {
+            return generateClassesString
+        }
+    }
+}
