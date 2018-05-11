fe69526f5d652ca8f37811faf89cd4acb584eea9
Seal <wusealking@gmail.com>
2018-05-11 13:45:22 +0800

    Fix issue #32

--- src/wu/seal/jsontokotlin/utils/SimplifiedMethods.kt
+++ src/wu/seal/jsontokotlin/utils/SimplifiedMethods.kt
@@ -25,7 +25,7 @@ fun executeCouldRollBackAction(project: Project?, action: (Project?) -> Unit) {
 /**
  * get the indent when generate kotlin class code
  */
-fun getIndent() :String{
+fun getIndent(): String {
 
     return buildString {
 
@@ -33,4 +33,34 @@ fun getIndent() :String{
             append(" ")
         }
     }
+}
+
+/**
+ * get class string block list from a big string which contains classes
+ */
+fun getClassesStringList(classesString: String): List<String> {
+    return classesString.split("\n\n").filter { it.isNotBlank() }
+}
+
+/**
+ * export the class name from class block string
+ */
+fun getClassNameFromClassBlockString(classBlockString: String): String {
+    return classBlockString.substringAfter("data class").substringBefore("(").trim()
+}
+
+fun replaceClassNameToClassBlockString(classBlockString: String, newClassName: String): String {
+    val blockPre = classBlockString.substringBefore("data class")
+    val blockAfter = classBlockString.substringAfter("(")
+    val blockMid = "data class $newClassName("
+    return blockPre + blockMid + blockAfter
+}
+
+fun <E, K, V> List<E>.toMap(converter: (E) -> Pair<K, V>): Map<K, V> {
+    val map = mutableMapOf<K, V>()
+    forEach {
+        val converterResult = converter(it)
+        map[converterResult.first] = converterResult.second
+    }
+    return map
 }
