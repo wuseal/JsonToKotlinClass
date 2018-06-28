bceb27059e6a548f1bce1f4c79cf6d3917a9d080
Seal <wusealking@gmail.com>
2018-06-28 23:24:50 +0800

    Release 2.2.0.1-eap update some code

--- src/main/kotlin/wu/seal/jsontokotlin/interceptor/MinimalAnnotationKotlinDataClassInterceptor.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/interceptor/MinimalAnnotationKotlinDataClassInterceptor.kt
@@ -4,6 +4,8 @@ import wu.seal.jsontokotlin.classscodestruct.KotlinDataClass
 
 /**
  * interceptor to make the code to be like the minimal annotation
+ * which means that if the property name is the same as raw name then remove the
+ * annotations contains %s
  */
 class MinimalAnnotationKotlinDataClassInterceptor() : IKotlinDataClassInterceptor {
 
@@ -27,14 +29,7 @@ class MinimalAnnotationKotlinDataClassInterceptor() : IKotlinDataClassIntercepto
             }
         }
 
-        return if (kotlinDataClass.nestedClasses.isNotEmpty()) {
-
-            val newNestedClasses = kotlinDataClass.nestedClasses.map { intercept(it) }
-            kotlinDataClass.copy(properties = newProperties, nestedClasses = newNestedClasses)
-
-        } else {
-            kotlinDataClass.copy(properties = newProperties)
-        }
+        return kotlinDataClass.copy(properties = newProperties)
 
     }
 
