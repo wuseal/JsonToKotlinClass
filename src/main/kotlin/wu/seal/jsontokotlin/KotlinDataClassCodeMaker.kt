d282f26119a2da52d7879b826d04c9d252f91449
Karthik <Karthik123>
2018-12-12 15:40:23 +0530

    Update - Fix issue#82 removed reuse of KotlinDataClassFileGenerator

--- src/main/kotlin/wu/seal/jsontokotlin/KotlinDataClassCodeMaker.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/KotlinDataClassCodeMaker.kt
@@ -2,9 +2,8 @@ package wu.seal.jsontokotlin
 
 import wu.seal.jsontokotlin.interceptor.IKotlinDataClassInterceptor
 import wu.seal.jsontokotlin.interceptor.InterceptorManager
-import com.intellij.psi.PsiDirectory
 
-class KotlinDataClassCodeMaker(private val rootClassName: String, private val json: String, private val directory: PsiDirectory) {
+class KotlinDataClassCodeMaker( private val rootClassName: String, private val json: String ) {
 
     fun makeKotlinDataClassCode(): String {
 
@@ -21,11 +20,9 @@ class KotlinDataClassCodeMaker(private val rootClassName: String, private val js
         return InterceptorManager.getEnabledKotlinDataClassInterceptors().isNotEmpty()
     }
 
-    fun makeKotlinDataClassCode(interceptors: List<IKotlinDataClassInterceptor>): String {
+    private fun makeKotlinDataClassCode(interceptors: List<IKotlinDataClassInterceptor>): String {
 
-        // added directory variable to check duplicate name classes in the folder
-        val kotlinDataClasses = KotlinDataClassMaker(rootClassName = rootClassName, json = json, directory = directory).makeKotlinDataClasses()
-
+        val kotlinDataClasses = KotlinDataClassMaker(rootClassName = rootClassName, json = json).makeKotlinDataClasses()
 
         val interceptedDataClasses = kotlinDataClasses.map {it.applyInterceptors(interceptors)}
         val code = interceptedDataClasses.joinToString("\n\n") {
--- src/main/kotlin/wu/seal/jsontokotlin/KotlinDataClassMaker.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/KotlinDataClassMaker.kt
@@ -1,15 +1,20 @@
 package wu.seal.jsontokotlin
 
+
+import wu.seal.jsontokotlin.codeelements.getDefaultValue
 import wu.seal.jsontokotlin.classscodestruct.KotlinDataClass
+import wu.seal.jsontokotlin.utils.*
+import wu.seal.jsontokotlin.utils.classblockparse.ClassCodeParser
 import wu.seal.jsontokotlin.utils.classblockparse.NestedClassModelClassesCodeParser
-import wu.seal.jsontokotlin.utils.classblockparse.NormalClassesCodeParser
-import com.intellij.psi.PsiDirectory
-import wu.seal.jsontokotlin.utils.ClassCodeFilter
-import wu.seal.jsontokotlin.utils.KotlinDataClassFileGenerator
 import wu.seal.jsontokotlin.utils.classblockparse.ParsedKotlinDataClass
 
-class KotlinDataClassMaker(private val rootClassName: String, private val json: String, private val directory: PsiDirectory) {
+class KotlinDataClassMaker(private val rootClassName: String, private val json: String) {
 
+
+    /**
+     * record the renamed class name when generate multiple files
+     */
+    private val renamedClassNames = mutableListOf<Pair<String, String>>()
 
     fun makeKotlinDataClasses(): List<KotlinDataClass> {
 
@@ -24,10 +29,153 @@ class KotlinDataClassMaker(private val rootClassName: String, private val json: 
 
             // reusing KotlinDataClassFileGenerator() to create the list of non duplicate classes
             val parsedKotlinClasses: List<ParsedKotlinDataClass> =
-                    KotlinDataClassFileGenerator().makeKotlinDataClasses(code, directory)
+                    makeKotlinDataClasses(code)
 
             return parsedKotlinClasses.map { KotlinDataClass.fromParsedKotlinDataClass(it) }
         }
+    }
+
+    // method to make in class data classes for JsonToKotlinClass, renames same class name to different
+    private fun makeKotlinDataClasses(
+            removeDuplicateClassCode: String) : List<ParsedKotlinDataClass>{
+
+        val kotlinClasses = getToBeGenerateClasses(removeDuplicateClassCode = removeDuplicateClassCode)
+
+        val notifyMessage = buildString {
+            append("${kotlinClasses.size} Kotlin Data Class files generated successful")
+            if (renamedClassNames.isNotEmpty()) {
+                append("\n")
+                append("These class names has been auto renamed to new names:\n ${renamedClassNames.map { it.first + " -> " + it.second }.toList()}")
+            }
+        }
+        showNotify(notifyMessage, null)
+
+
+        return kotlinClasses
+    }
+
+    private fun getToBeGenerateClasses(removeDuplicateClassCode: String) : List<ParsedKotlinDataClass>
+    {
+        val classes =
+                getClassesStringList(removeDuplicateClassCode).map { ClassCodeParser(it).getKotlinDataClass() }
+
+        /**
+         * Build Property Type reference to ParsedKotlinDataClass
+         * Only pre class property type could reference behind classes
+         */
+        val buildRefClasses = buildTypeReference(classes)
+
+        val newClassNames = getNoneConflictClassNames(buildRefClasses)
+
+        val newKotlinClasses = updateClassNames(buildRefClasses, newClassNames)
+
+
+        return synchronizedPropertyTypeWithTypeRef(newKotlinClasses)
+
+    }
+
+
+    /**
+     * None conflict with current directory files and exist class
+     */
+    private fun getNoneConflictClassNames(
+            buildRefClasses: List<ParsedKotlinDataClass>): List<String> {
+
+        val resolveSameConflictClassesNames = mutableListOf<String>()
+        buildRefClasses.forEach {
+            val originClassName = it.name
+            val newClassName = changeClassNameIfCurrentListContains(resolveSameConflictClassesNames, originClassName)
+            resolveSameConflictClassesNames.add(newClassName)
+        }
+
+        return resolveSameConflictClassesNames
+    }
+
+
+
+    private fun updateClassNames(
+            dataClasses: List<ParsedKotlinDataClass>,
+            newClassNames: List<String>
+    ): List<ParsedKotlinDataClass> {
+
+        val newKotlinClasses = dataClasses.toMutableList()
+
+        newKotlinClasses.forEachIndexed { index, kotlinDataClass ->
+
+            val newClassName = newClassNames[index]
+            val originClassName = kotlinDataClass.name
+
+            if (newClassName != originClassName) {
+                renamedClassNames.add(Pair(originClassName, newClassName))
+                val newKotlinDataClass = kotlinDataClass.copy(name = newClassName)
+                newKotlinClasses[index] = newKotlinDataClass
+                updateTypeRef(dataClasses, kotlinDataClass, newKotlinDataClass)
+            }
+        }
+
+        return newKotlinClasses
+    }
+
+    private fun updateTypeRef(
+            classes: List<ParsedKotlinDataClass>,
+            originDataClass: ParsedKotlinDataClass,
+            newKotlinDataClass: ParsedKotlinDataClass
+    ) {
+        classes.forEach {
+            it.properties.forEach {
+                if (it.kotlinDataClassPropertyTypeRef == originDataClass) {
+                    it.kotlinDataClassPropertyTypeRef = newKotlinDataClass
+                }
+            }
+        }
+    }
+
+    private fun synchronizedPropertyTypeWithTypeRef(unSynchronizedTypeClasses: List<ParsedKotlinDataClass>): List<ParsedKotlinDataClass> {
+        return unSynchronizedTypeClasses.map { dataClass: ParsedKotlinDataClass ->
+
+            val newProperties = dataClass.properties.map { it ->
+                if (it.kotlinDataClassPropertyTypeRef != ParsedKotlinDataClass.NONE) {
+                    val rawPropertyReferenceType = getRawType(getChildType(it.propertyType))
+                    val tobeReplaceNewType =
+                            it.propertyType.replace(rawPropertyReferenceType, it.kotlinDataClassPropertyTypeRef.name)
+                    if (it.propertyValue.isNotBlank()) {
+                        it.copy(propertyType = tobeReplaceNewType, propertyValue = getDefaultValue(tobeReplaceNewType))
+                    } else
+                        it.copy(propertyType = tobeReplaceNewType)
+                } else {
+                    it
+                }
+            }
+            dataClass.copy(properties = newProperties)
+        }
+    }
+
+    private fun buildTypeReference(classes: List<ParsedKotlinDataClass>): List<ParsedKotlinDataClass> {
+        val classNameList = classes.map { it.name }
+
+        /**
+         * Build Property Type reference to ParsedKotlinDataClass
+         * Only pre class property type could reference behind classes
+         */
+        classes.forEachIndexed { index, kotlinDataClass ->
+            kotlinDataClass.properties.forEachIndexed { _, property ->
+                val indexOfClassName =
+                        classNameList.firstIndexAfterSpecificIndex(getRawType(getChildType(property.propertyType)), index)
+                if (indexOfClassName != -1) {
+                    property.kotlinDataClassPropertyTypeRef = classes[indexOfClassName]
+                }
+            }
+        }
+
+        return classes
+    }
+
+    private fun changeClassNameIfCurrentListContains(classesNames: List<String>, className: String): String {
+        var newClassName = className
+        while (classesNames.contains(newClassName)) {
+            newClassName += "X"
+        }
+        return newClassName
     }
 
 }
