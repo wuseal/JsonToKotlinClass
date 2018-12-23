9902bc672a2665f82b8521e855c311243255b7e9
Seal <wusealking@gmail.com>
2018-12-23 22:17:32 +0800

    Fix bug #83
    (cherry picked from commit 79b22d7eeb4cb333899bd85e32cb3d6b416b497b)

--- src/main/kotlin/wu/seal/jsontokotlin/MakeKotlinClassAction.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/MakeKotlinClassAction.kt
@@ -16,6 +16,7 @@ import wu.seal.jsontokotlin.feedback.sendActionInfo
 import wu.seal.jsontokotlin.ui.JsonInputDialog
 import wu.seal.jsontokotlin.utils.ClassCodeFilter
 import wu.seal.jsontokotlin.utils.executeCouldRollBackAction
+import java.awt.SystemColor.text
 import java.net.URL
 import java.util.*
 
@@ -144,6 +145,23 @@ class MakeKotlinClassAction : AnAction("MakeKotlinClass") {
                 if (offset == 0) {
                     offset = document.textLength
                 }
+                val lastPackageKeywordLineEndIndex = try {
+                    "^[\\s]*package.+\n$".toRegex(RegexOption.MULTILINE).findAll(document.text).last().range.endInclusive
+                } catch (e: Exception) {
+                    -1
+                }
+                val lastImportKeywordLineEndIndex = try {
+                    "^[\\s]*import.+\n$".toRegex(RegexOption.MULTILINE).findAll(document.text).last().range.endInclusive
+                } catch (e: Exception) {
+                    -1
+                }
+                if (offset < lastPackageKeywordLineEndIndex) {
+                    offset = lastPackageKeywordLineEndIndex + 1
+                }
+                if (offset < lastImportKeywordLineEndIndex) {
+                    offset = lastImportKeywordLineEndIndex + 1
+                }
+
             } else {
                 offset = document.textLength
             }
