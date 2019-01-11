a4085cc386b50c33a63bff6151cbafc569161783
kezhenxu94 <kezhenxu94@163.com>
2019-01-11 23:12:54 +0800

    bugfix: fix #94

--- src/test/kotlin/wu/seal/jsontokotlin/regression/Issue094Test.kt
+++ src/test/kotlin/wu/seal/jsontokotlin/regression/Issue094Test.kt
@@ -1,9 +1,8 @@
 package wu.seal.jsontokotlin.regression
 
 import com.winterbe.expekt.should
-import org.junit.Before
 import org.junit.Test
-import wu.seal.jsontokotlin.KotlinCodeMaker
+import wu.seal.jsontokotlin.KotlinDataClassCodeMaker
 import wu.seal.jsontokotlin.test.TestConfig
 
 /**
@@ -15,22 +14,19 @@ class Issue094Test {
     private val testJson = """
     { "test": {} }
     """.trimIndent()
-    private val expected = """
-data class A(
-    @SerializedName("test") val test: Test = Test()
+    private val expected = """data class A(
+    @SerializedName("test")
+    val test: Test = Test()
 ) {
-    
     class Test(
     )
 }"""
 
-    @Before
-    fun setUp() {
-        TestConfig.setToTestInitState()
-    }
-
     @Test
-    fun testIssue087() {
-        KotlinCodeMaker("A", testJson).makeKotlinData().should.be.equal(expected)
+    fun testIssue094() {
+        TestConfig.setToTestInitState()
+        val generated = KotlinDataClassCodeMaker("A", testJson).makeKotlinDataClassCode()
+        println("generated = ${generated}")
+        generated.should.be.equal(expected)
     }
 }
