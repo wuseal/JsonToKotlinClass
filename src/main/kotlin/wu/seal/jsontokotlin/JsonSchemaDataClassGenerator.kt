36a4fb8ef9d589ba0286e84d0679f49bc631bc63
kezhenxu94 <kezhenxu94@163.com>
2019-03-29 22:23:00 +0800

    support generating class from JsonSchema

--- src/main/kotlin/wu/seal/jsontokotlin/JsonSchemaDataClassGenerator.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/JsonSchemaDataClassGenerator.kt
@@ -8,14 +8,18 @@ class JsonSchemaDataClassGenerator(private val jsonObjectDef: JsonObjectDef) {
 
   fun generate(className: String?) {
     val clazz = className
-      ?: jsonObjectDef.title
-      ?: throw IllegalArgumentException("className cannot be null when jsonObjectDef.title is null")
+        ?: jsonObjectDef.title
+        ?: throw IllegalArgumentException("className cannot be null when jsonObjectDef.title is null")
 
     val requiredFields = jsonObjectDef.required
-    val s = TypeSpec.classBuilder(clazz)
-      .addModifiers(KModifier.DATA)
-      .primaryConstructor(FunSpec.constructorBuilder().apply {
-        jsonObjectDef.properties.forEach { property, propertyDefinition ->
+    val properties = if (ConfigManager.isOrderByAlphabetical) jsonObjectDef.properties.toSortedMap() else jsonObjectDef.properties
+    val s = TypeSpec.classBuilder(clazz).apply {
+      if (!ConfigManager.isCommentOff) {
+        addKdoc(jsonObjectDef.description)
+      }
+      addModifiers(KModifier.DATA)
+      primaryConstructor(FunSpec.constructorBuilder().apply {
+        properties.forEach { property, propertyDefinition ->
           val type = JSON_SCHEMA_TYPE_MAPPINGS[propertyDefinition.type] ?: String::class
           if (type == Any::class) {
             val typeName = if (property !in requiredFields) {
@@ -33,24 +37,38 @@ class JsonSchemaDataClassGenerator(private val jsonObjectDef: JsonObjectDef) {
             addParameter(property, typeName)
           }
         }
-      }.build()).apply {
-        jsonObjectDef.properties.forEach { property, propertyDefinition ->
-          val type = JSON_SCHEMA_TYPE_MAPPINGS[propertyDefinition.type] ?: String::class
-          if (type == Any::class) {
-            val typeName = if (property !in requiredFields) {
-              ClassName("", property.capitalize()).copy(nullable = true)
-            } else ClassName("", property.capitalize())
-            addProperty(PropertySpec.builder(property, typeName).initializer(property).build())
-          } else {
-            val typeName = if (property !in requiredFields) {
-              type.asTypeName().copy(nullable = true)
-            } else type.asTypeName()
+      }.build())
+    }.apply {
+      properties.forEach { property, propertyDefinition ->
+        val type = JSON_SCHEMA_TYPE_MAPPINGS[propertyDefinition.type] ?: String::class
+        if (type == Any::class) {
+          val typeName = if (property !in requiredFields) {
+            ClassName("", property.capitalize()).copy(nullable = true)
+          } else ClassName("", property.capitalize())
+          addProperty(
+              PropertySpec.builder(property, typeName).apply {
+                if (!ConfigManager.isCommentOff) {
+                  addKdoc(propertyDefinition.description)
+                }
+                initializer(property)
+              }.build()
+          )
+        } else {
+          val typeName = if (property !in requiredFields) {
+            type.asTypeName().copy(nullable = true)
+          } else type.asTypeName()
 
-            addProperty(PropertySpec.builder(property, typeName).initializer(property).build())
-          }
+          addProperty(
+              PropertySpec.builder(property, typeName).apply {
+                if (!ConfigManager.isCommentOff) {
+                  addKdoc(propertyDefinition.description)
+                }
+                initializer(property)
+              }.build()
+          )
         }
       }
-      .build()
+    }.build()
     classes.add(s)
   }
 }
