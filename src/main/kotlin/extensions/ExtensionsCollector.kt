1e64f1bd17e9bf97f504f5241198ba128542c3bd
Ted <tedzeng@iresearch.com.cn>
2019-06-13 18:38:19 +0800

    issue #169 fixed

--- src/main/kotlin/extensions/ExtensionsCollector.kt
+++ src/main/kotlin/extensions/ExtensionsCollector.kt
@@ -1,8 +1,10 @@
 package extensions
 
+import extensions.chen.biao.KeepAnnotationSupport
 import extensions.wu.seal.PropertyPrefixSupport
 import extensions.wu.seal.PropertySuffixSupport
-import extensions.chen.biao.KeepAnnotationSupport
+import extensions.ted.zeng.PropertyAnnotationLineSupport
+
 /**
  * extension collect, all extensions will be hold by this class's extensions property
  */
@@ -13,6 +15,7 @@ object ExtensionsCollector {
     val extensions = listOf(
             PropertyPrefixSupport,
             PropertySuffixSupport,
-            KeepAnnotationSupport
+            KeepAnnotationSupport,
+            PropertyAnnotationLineSupport
     )
 }
