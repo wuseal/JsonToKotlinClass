fef065a3caafb065eb530bf26812b9c9b03c7aed
Ted <tedzeng@iresearch.com.cn>
2019-06-14 13:55:27 +0800

    issue #169 fixed

--- src/main/kotlin/wu/seal/jsontokotlin/classscodestruct/Property.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/classscodestruct/Property.kt
@@ -15,13 +15,19 @@ data class Property(
         val originJsonValue: String? = ""
 ) {
 
+    private var separatorBetweenAnnotationAndProperty = "\n"
+
+    fun letLastAnnotationStayInSameLine() {
+        separatorBetweenAnnotationAndProperty = " "
+    }
+
     fun getCode(): String {
 
         return buildString {
             if (annotations.isNotEmpty()) {
                 val annotationsCode = annotations.joinToString("\n") { it.getAnnotationString() }
                 if (annotationsCode.isNotBlank()) {
-                    append(annotationsCode).append("\n")
+                    append(annotationsCode).append(separatorBetweenAnnotationAndProperty)
                 }
             }
             append(keyword).append(" ").append(name).append(": ").append(type)
