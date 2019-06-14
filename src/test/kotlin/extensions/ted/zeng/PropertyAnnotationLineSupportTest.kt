c05630e5b9a2794dfaecde8733d4eae4519f48b6
Ted <linzeng1990@foxmail.com>
2019-06-14 19:10:29 +0800

    issue #169 fixed (#186)

---
+++ src/test/kotlin/extensions/ted/zeng/PropertyAnnotationLineSupportTest.kt
@@ -0,0 +1,33 @@
+package extensions.ted.zeng
+
+import com.winterbe.expekt.should
+import org.junit.Before
+import org.junit.Test
+import wu.seal.jsontokotlin.generateKotlinDataClass
+import wu.seal.jsontokotlin.interceptor.annotations.fastjson.AddFastJsonAnnotationInterceptor
+import wu.seal.jsontokotlin.test.TestConfig
+
+/**
+ * Created by ted on 2019-06-13 18:21.
+ */
+class PropertyAnnotationLineSupportTest {
+    private val json = """{"a":"a","Int":2}"""
+    private val expectResult = """data class Test(
+    @JSONField(name = "a") val a: String, // a
+    @JSONField(name = "Int") val int: Int // 2
+)"""
+
+    @Before
+    fun setUp() {
+        TestConfig.setToTestInitState()
+    }
+
+    @Test
+    fun interceptTest() {
+        val kotlinDataClass = json.generateKotlinDataClass()
+        PropertyAnnotationLineSupport.getTestHelper().setConfig("ted.zeng.property_annotation_in_same_line_enable", "true")
+        val result = kotlinDataClass.applyInterceptors(listOf(AddFastJsonAnnotationInterceptor(), PropertyAnnotationLineSupport)).getCode()
+        result.should.equal(expectResult)
+    }
+
+}
