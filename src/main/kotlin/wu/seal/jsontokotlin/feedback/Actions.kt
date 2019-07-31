c8c7314243d7574dd116068f01aedbf890726757
Seal <wusealking@gmail.com>
2019-07-31 22:05:33 +0800

    add click url feedback

--- src/main/kotlin/wu/seal/jsontokotlin/feedback/Actions.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/feedback/Actions.kt
@@ -11,6 +11,7 @@ import java.util.*
 const val ACTION_START = "action_start"
 const val ACTION_SUCCESS_COMPLETE = "action_success_complete"
 const val ACTION_FORMAT_JSON = "action_format_json"
+const val ACTION_CLICK_PROJECT_URL = "action_click_project_url"
 
 data class StartAction(
     val uuid: String = UUID,
@@ -34,4 +35,12 @@ data class FormatJSONAction(
     val actionType: String = ACTION_FORMAT_JSON,
     val time: String = Date().time.toString(),
     val daytime: String = SimpleDateFormat("yyyy-MM-dd", Locale.CHINA).format(Date())
+)
+
+data class ClickProjectURLAction(
+        val uuid: String = UUID,
+        val pluginVersion: String = PLUGIN_VERSION,
+        val actionType: String = ACTION_CLICK_PROJECT_URL,
+        val time: String = Date().time.toString(),
+        val daytime: String = SimpleDateFormat("yyyy-MM-dd", Locale.CHINA).format(Date())
 )
