c6d32e5530390b7788967e9f02e74aefe305c030
Ted <linzeng1990@foxmail.com>
2019-08-27 16:24:46 +0800

    complete unit test FinalKotlinDataClassWrapperInterceptorTest (#235)

--- src/test/kotlin/wu/seal/jsontokotlin/interceptor/FinalKotlinDataClassWrapperInterceptorTest.kt
+++ src/test/kotlin/wu/seal/jsontokotlin/interceptor/FinalKotlinDataClassWrapperInterceptorTest.kt
@@ -1,7 +1,9 @@
 package wu.seal.jsontokotlin.interceptor
 
+import com.winterbe.expekt.should
 import org.junit.Before
 import org.junit.Test
+import wu.seal.jsontokotlin.generateKotlinDataClass
 import wu.seal.jsontokotlin.test.TestConfig
 
 class FinalKotlinDataClassWrapperInterceptorTest {
@@ -13,22 +15,15 @@ class FinalKotlinDataClassWrapperInterceptorTest {
 
     @Test
     fun intercept() {
-//        val toBeParsedCode = """data class A(val in: String,
-//            val as: Int,
-//            val dupa: Double,
-//            val 1ab: Long,
-//            val ab_c$""" + 'd' + """: String,
-//            val : Int
-//)"""
-//        val kotlinDataClass = KotlinDataClass.fromParsedKotlinDataClass(ClassCodeParser(toBeParsedCode).getKotlinDataClass())
-//        val intercepted = FinalKotlinDataClassWrapperInterceptor().intercept(kotlinDataClass)
-//        intercepted.getCode().should.be.equal("""data class A(
-//    val `in`: String,
-//    val `as`: Int,
-//    val dupa: Double,
-//    val `1ab`: Long,
-//    val `ab_c$"""+"d`"+""": String,
-//    val : Int
-//)""")
+        with("""{"in":"","as":1,"dupa":1.0,"1ab":99999999999999,"ab_c${"$"}d":"","":1}""") {
+            generateKotlinDataClass("A").applyInterceptors(listOf(CommentOffInterceptor, FinalKotlinDataClassWrapperInterceptor()))
+        }.getCode().should.be.equal("""data class A(
+    val `in`: String,
+    val `as`: Int,
+    val dupa: Double,
+    val `1ab`: Long,
+    val `ab_c${"$"}d`: String,
+    val : Int
+)""")
     }
 }
