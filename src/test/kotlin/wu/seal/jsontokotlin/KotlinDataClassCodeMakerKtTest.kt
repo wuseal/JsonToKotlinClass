e0ef7bf8e59a6eccc47b7fc6a34477623d6dd9a4
Seal <wusealking@gmail.com>
2019-08-31 21:23:27 +0800

    fix an exception generate split class code with json: ```json { "text": "MXCHIP won a prize", "id":1234, "detail": {      "comp":"MXCHIP.Inc",      "from":"Shanghai",      "focus":"Internet of Things",      "module":[{"k":"EMW3165"},{"k":"EMW3166"},{"k":"EMW3167"},{"k":"EMW3168"}]    } } ```

--- src/test/kotlin/wu/seal/jsontokotlin/KotlinDataClassCodeMakerKtTest.kt
+++ src/test/kotlin/wu/seal/jsontokotlin/KotlinDataClassCodeMakerKtTest.kt
@@ -1,10 +1,8 @@
 package wu.seal.jsontokotlin
 
 import com.winterbe.expekt.should
-import org.junit.Test
-
-import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
 import wu.seal.jsontokotlin.test.TestConfig
 import wu.seal.jsontokotlin.utils.TYPE_STRING
 
@@ -73,5 +71,72 @@ class KotlinDataClassCodeMakerKtTest {
         dataClass.properties[2].typeObject!!.name.should.be.equal("C")
         dataClass.properties[2].typeObject!!.properties[1].type.should.be.equal("CX")
         dataClass.properties[2].typeObject!!.properties[1].typeObject!!.name.should.be.equal("CX")
+    }
+
+    @Test
+    fun resolveInnerConflictClassName2() {
+        val json = """
+        {
+        "text": "MXCHIP won a prize",
+        "id":1234,
+        "detail": {
+             "comp":"MXCHIP.Inc",
+             "from":"Shanghai",
+             "focus":"Internet of Things",
+             "module":[{"k":"EMW3165"},{"k":"EMW3166"},{"k":"EMW3167"},{"k":"EMW3168"}]
+           }
+        }
+        """.trimIndent()
+        val dataClass = json.generateKotlinDataClass("Test").resolveInnerConflictClassName()
+        dataClass.run {
+            name.should.be.equal("Test")
+            properties.size.should.be.equal(3)
+            properties[0].run {
+                name.should.be.equal("text")
+                originJsonValue.should.be.equal("MXCHIP won a prize")
+            }
+            properties[1].run {
+                name.should.be.equal("id")
+                originJsonValue.should.be.equal("1234")
+            }
+            properties[2].run {
+                name.should.be.equal("detail")
+                type.should.be.equal("Detail")
+                originJsonValue.should.be.equal("")
+                typeObject.should.not.be.`null`
+                typeObject!!.run {
+                    name.should.be.equal("Detail")
+                    properties.size.should.be.equal(4)
+                    properties[0].run {
+                        name.should.be.equal("comp")
+                        originJsonValue.should.equal("MXCHIP.Inc")
+                    }
+                    properties[1].run {
+                        name.should.be.equal("from")
+                        originJsonValue.should.equal("Shanghai")
+                    }
+                    properties[2].run {
+                        name.should.be.equal("focus")
+                        originJsonValue.should.equal("Internet of Things")
+                    }
+                    properties[3].run {
+                        name.should.be.equal("module")
+                        originJsonValue.should.equal("")
+                        type.should.be.equal("List<Module>")
+                        typeObject.should.not.be.`null`
+                        typeObject!!.run {
+                            name.should.be.equal("Module")
+                            properties.size.should.be.equal(1)
+                            properties[0].run {
+                                name.should.be.equal("k")
+                                originJsonValue.should.be.equal("EMW3168")
+                                type.should.be.equal(TYPE_STRING)
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
     }
 }
