8766549f143c5711adaf5b89056c16be7c4bc3f8
Bimo <xr1995@aliyun.com>
2019-09-01 23:17:24 -0500

    add extension force primitive type non-nullable (#239)
    * add extension force primitive type non-nullable
    
    * fix test class type

--- src/main/kotlin/extensions/ExtensionsCollector.kt
+++ src/main/kotlin/extensions/ExtensionsCollector.kt
@@ -1,11 +1,12 @@
 package extensions
 
 import extensions.chen.biao.KeepAnnotationSupport
+import extensions.jose.han.ParcelableAnnotationSupport
 import extensions.ted.zeng.PropertyAnnotationLineSupport
 import extensions.wu.seal.ClassNameSuffixSupport
 import extensions.wu.seal.PropertyPrefixSupport
 import extensions.wu.seal.PropertySuffixSupport
-import extensions.jose.han.ParcelableAnnotationSupport
+import extensions.xu.rui.PrimitiveTypeNonNullableSupport
 
 /**
  * extension collect, all extensions will be hold by this class's extensions property
@@ -20,6 +21,7 @@ object ExtensionsCollector {
             ParcelableAnnotationSupport,
             PropertyPrefixSupport,
             PropertySuffixSupport,
-            ClassNameSuffixSupport
+            ClassNameSuffixSupport,
+            PrimitiveTypeNonNullableSupport
     )
 }
