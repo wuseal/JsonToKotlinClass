032b5009b4e1cff09a33e14818d7b4815b729e3d
Seal <wusealking@gmail.com>
2019-11-09 15:27:17 +0800

    fix #84

--- src/main/kotlin/extensions/ExtensionsCollector.kt
+++ src/main/kotlin/extensions/ExtensionsCollector.kt
@@ -4,6 +4,7 @@ import extensions.chen.biao.KeepAnnotationSupport
 import extensions.jose.han.ParcelableAnnotationSupport
 import extensions.ted.zeng.PropertyAnnotationLineSupport
 import extensions.wu.seal.ClassNameSuffixSupport
+import extensions.wu.seal.ForceInitDefaultValueWithOriginJsonValueSupport
 import extensions.wu.seal.PropertyPrefixSupport
 import extensions.wu.seal.PropertySuffixSupport
 import extensions.xu.rui.PrimitiveTypeNonNullableSupport
@@ -16,12 +17,13 @@ object ExtensionsCollector {
      * all extensions
      */
     val extensions = listOf(
-            KeepAnnotationSupport,
-            PropertyAnnotationLineSupport,
-            ParcelableAnnotationSupport,
-            PropertyPrefixSupport,
-            PropertySuffixSupport,
-            ClassNameSuffixSupport,
-            PrimitiveTypeNonNullableSupport
+        KeepAnnotationSupport,
+        PropertyAnnotationLineSupport,
+        ParcelableAnnotationSupport,
+        PropertyPrefixSupport,
+        PropertySuffixSupport,
+        ClassNameSuffixSupport,
+        PrimitiveTypeNonNullableSupport,
+        ForceInitDefaultValueWithOriginJsonValueSupport
     )
 }
