252d38bfb50fe331d41e8174c47f5532a8e359c9
theapache64 <theapache64@gmail.com>
2020-01-06 18:15:23 +0530

    ➕ add global exception handler

--- api/src/main/kotlin/wu/seal/jsontokotlin/server/controllers/GenerateController.kt
+++ api/src/main/kotlin/wu/seal/jsontokotlin/server/controllers/GenerateController.kt
@@ -1,24 +1,26 @@
 package wu.seal.jsontokotlin.server.controllers
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
-import wu.seal.jsontokotlin.server.routes.generate.Data
+import org.springframework.web.bind.annotation.ResponseStatus
 import wu.seal.jsontokotlin.server.routes.generate.GenerateRequest
 import wu.seal.jsontokotlin.server.routes.generate.GenerateResponse
 
-@Controller()
+@Controller
 class GenerateController {
 
     @PostMapping("/api/generate")
     @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
     fun generate(@RequestBody request: GenerateRequest): GenerateResponse {
         return GenerateResponse(
-                Data("This is code"),
+                GenerateResponse.Data("some code goes here"),
                 true,
-                123,
-                "Sample message"
+                -1,
+                "OK"
         )
     }
 }
--- api/src/main/kotlin/wu/seal/jsontokotlin/server/routes/generate/GenerateResponse.kt
+++ api/src/main/kotlin/wu/seal/jsontokotlin/server/routes/generate/GenerateResponse.kt
@@ -1,20 +1,14 @@
 package wu.seal.jsontokotlin.server.routes.generate
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import wu.seal.jsontokotlin.server.routes.base.BaseResponse
 
 
-data class GenerateResponse(
-        @JsonProperty("data")
-        val `data`: Data,
-        @JsonProperty("error")
-        val error: Boolean, // false
-        @JsonProperty("error_code")
-        val errorCode: Int, // 123
-        @JsonProperty("message")
-        val message: String // Generated
-)
+class GenerateResponse(data: Data, error: Boolean, errorCode: Int, message: String) :
+        BaseResponse<GenerateResponse.Data>(data, error, errorCode, message) {
+    data class Data(
+            @field:JsonProperty("code")
+            val code: String // The code goes here
+    )
+}
 
-data class Data(
-        @JsonProperty("code")
-        val code: String // The code goes here
-)
