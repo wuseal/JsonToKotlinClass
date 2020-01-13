ee301c0b6f1a7f97c4b4079306f0176a97225678
theapache64 <theapache64@gmail.com>
2020-01-13 12:06:29 +0530

    ☑️ add test cases

--- api/src/main/kotlin/wu/seal/jsontokotlin/server/routes/generate/GenerateRequest.kt
+++ api/src/main/kotlin/wu/seal/jsontokotlin/server/routes/generate/GenerateRequest.kt
@@ -7,61 +7,61 @@ class GenerateRequest(
         val json: String,
 
         @JsonProperty("class_name")
-        val className : String,
+        val className: String,
 
         @JsonProperty("annotation_lib")
-        val annotationLib: String?, // MOSHI_CODE_GEN
+        val annotationLib: String? = null, // MOSHI_CODE_GEN
 
         @JsonProperty("class_suffix")
-        val classSuffix: String?, // MY_CLASS_SUFFIX
+        val classSuffix: String? = null, // MY_CLASS_SUFFIX
 
         @JsonProperty("default_value_strategy")
-        val defaultValueStrategy: String?, // AVOID_NULL
+        val defaultValueStrategy: String? = null, // AVOID_NULL
 
         @JsonProperty("indent")
-        val indent: Int?, // 3
+        val indent: Int? = null, // 3
 
         @JsonProperty("is_comments_enabled")
-        val isCommentsEnabled: Boolean?, // true
+        val isCommentsEnabled: Boolean? = null, // true
 
         @JsonProperty("is_create_annotation_only_when_needed_enabled")
-        val isCreateAnnotationOnlyWhenNeededEnabled: Boolean?, // true
+        val isCreateAnnotationOnlyWhenNeededEnabled: Boolean? = null, // true
 
         @JsonProperty("is_enable_var_properties")
-        val isEnableVarProperties: Boolean?, // false
+        val isEnableVarProperties: Boolean? = null, // false
 
         @JsonProperty("is_force_init_default_value_with_origin_json_value_enabled")
-        val isForceInitDefaultValueWithOriginJsonValueEnabled: Boolean?, // true
+        val isForceInitDefaultValueWithOriginJsonValueEnabled: Boolean? = null, // true
 
         @JsonProperty("is_force_primitive_type_non_nullable_enabled")
-        val isForcePrimitiveTypeNonNullableEnabled: Boolean?, // true
+        val isForcePrimitiveTypeNonNullableEnabled: Boolean? = null, // true
 
         @JsonProperty("is_inner_class_model_enabled")
-        val isInnerClassModelEnabled: Boolean?, // false
+        val isInnerClassModelEnabled: Boolean? = null, // false
 
         @JsonProperty("is_keep_annotation_on_class_androidx_enabled")
-        val isKeepAnnotationOnClassAndroidXEnabled: Boolean?, // true
+        val isKeepAnnotationOnClassAndroidXEnabled: Boolean? = null, // true
 
         @JsonProperty("is_keep_annotation_on_class_enabled")
-        val isKeepAnnotationOnClassEnabled: Boolean?, // true
+        val isKeepAnnotationOnClassEnabled: Boolean? = null, // true
 
         @JsonProperty("is_map_type_enabled")
-        val isMapTypeEnabled: Boolean?, // true
+        val isMapTypeEnabled: Boolean? = null, // true
 
         @JsonProperty("is_order_by_alphabetic_enabled")
-        val isOrderByAlphabeticEnabled: Boolean?, // true
+        val isOrderByAlphabeticEnabled: Boolean? = null, // true
         @JsonProperty("is_parcelable_support_enabled")
-        val isParcelableSupportEnabled: Boolean?, // true
+        val isParcelableSupportEnabled: Boolean? = null, // true
         @JsonProperty("is_property_and_annotation_in_same_line_enabled")
-        val isPropertyAndAnnotationInSameLineEnabled: Boolean?, // true
+        val isPropertyAndAnnotationInSameLineEnabled: Boolean? = null, // true
         @JsonProperty("package_name")
-        val packageName: String?, // com.my.package.name
+        val packageName: String? = null, // com.my.package.name
         @JsonProperty("parent_class_template")
-        val parentClassTemplate: String?, // android.os.Parcelable
+        val parentClassTemplate: String? = null, // android.os.Parcelable
         @JsonProperty("property_prefix")
-        val propertyPrefix: String?, // MY_PREFIX
+        val propertyPrefix: String? = null, // MY_PREFIX
         @JsonProperty("property_suffix")
-        val propertySuffix: String?, // MY_SUFFIX
+        val propertySuffix: String? = null, // MY_SUFFIX
         @JsonProperty("property_type_strategy")
-        val propertyTypeStrategy: String? // AUTO_DETERMINE_NULLABLE_OR_NOT
+        val propertyTypeStrategy: String? = null // AUTO_DETERMINE_NULLABLE_OR_NOT
 )
