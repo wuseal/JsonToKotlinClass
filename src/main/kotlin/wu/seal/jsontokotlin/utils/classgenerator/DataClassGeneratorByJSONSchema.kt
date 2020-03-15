12561d4aec2366bba04b0c7f2e7a19dcd87215b1
TedZen <linzeng1990@foxmail.com>
2020-03-15 22:04:47 +0800

    merge JsonSchemaDataClassGenerator.kt into DataClassGeneratorByJSONSchema.kt with kotlinpoet-removing

--- src/main/kotlin/wu/seal/jsontokotlin/utils/classgenerator/DataClassGeneratorByJSONSchema.kt
+++ src/main/kotlin/wu/seal/jsontokotlin/utils/classgenerator/DataClassGeneratorByJSONSchema.kt
@@ -1,110 +1,85 @@
 package wu.seal.jsontokotlin.utils.classgenerator
 
+import wu.seal.jsontokotlin.JSON_SCHEMA_FORMAT_MAPPINGS
+import wu.seal.jsontokotlin.model.classscodestruct.*
+import wu.seal.jsontokotlin.model.jsonschema.JsonObjectDef
 import wu.seal.jsontokotlin.model.jsonschema.JsonSchema
-import wu.seal.jsontokotlin.model.classscodestruct.GenericListClass
-import wu.seal.jsontokotlin.model.classscodestruct.KotlinClass
-import wu.seal.jsontokotlin.model.classscodestruct.DataClass
-import wu.seal.jsontokotlin.model.classscodestruct.Property
-import wu.seal.jsontokotlin.model.codeelements.getDefaultValue
-import wu.seal.jsontokotlin.utils.*
+import wu.seal.jsontokotlin.model.jsonschema.PropertyDef
 
 class DataClassGeneratorByJSONSchema(private val rootClassName: String, private val jsonSchema: JsonSchema) {
 
     fun generate(): DataClass {
-        val requires = jsonSchema.required
-        val properties = mutableListOf<Property>()
-        jsonSchema.properties.forEach { (name, schema) ->
-            when (schema.type) {
-                "object" -> {
-                    val dataClass = DataClassGeneratorByJSONSchema(name, schema).generate()
-                    val value = if (requires.contains(name)) getDefaultValue(name) else null
-                    val property =
-                            Property(
-                                    originName = name,
-                                    originJsonValue = value,
-                                    type = name,
-                                    typeObject = dataClass,
-                                    comment = schema.description ?: ""
-                            )
-                    properties.add(property)
-                }
-                "string" -> {
-                    val value = if (requires.contains(name)) getDefaultValue(TYPE_STRING) else null
-                    val property = Property(
-                            originName = name,
-                            originJsonValue = value,
-                            type = KotlinClass.STRING.name,
-                            comment = schema.description ?: "",
-                            typeObject = KotlinClass.STRING
-                    )
-                    properties.add(property)
-                }
-                "enum" -> {
-                    val value = if (requires.contains(name)) getDefaultValue(TYPE_STRING) else null
-                    val property = Property(
-                            originName = name,
-                            originJsonValue = value,
-                            type = KotlinClass.STRING.name,
-                            comment = schema.description ?: "",
-                            typeObject = KotlinClass.STRING
-                    )
-                    properties.add(property)
-                }
-                "integer" -> {
-                    val value = if (requires.contains(name)) getDefaultValue(TYPE_INT) else null
-                    val property = Property(
-                            originName = name,
-                            originJsonValue = value,
-                            type = KotlinClass.INT.name,
-                            comment = schema.description ?: "",
-                            typeObject = KotlinClass.INT
-                    )
-                    properties.add(property)
-                }
-                "number" -> {
-                    val value = if (requires.contains(name)) getDefaultValue(TYPE_DOUBLE) else null
-                    val property = Property(
-                            originName = name,
-                            originJsonValue = value,
-                            type = KotlinClass.DOUBLE.name,
-                            comment = schema.description ?: "",
-                            typeObject = KotlinClass.DOUBLE
-                    )
-                    properties.add(property)
-                }
-                "boolean" -> {
-                    val value = if (requires.contains(name)) getDefaultValue(TYPE_BOOLEAN) else null
-                    val property = Property(
-                            originName = name,
-                            originJsonValue = value,
-                            type = KotlinClass.BOOLEAN.name,
-                            comment = schema.description ?: "",
-                            typeObject = KotlinClass.BOOLEAN
-                    )
-                    properties.add(property)
-                }
-                "array" -> {
-                    val itemType = getItemTypeOfArray(schema)
-                    val type = GenericListClass(generic = itemType)
-                    val value = if (requires.contains(name)) getDefaultValue(type.name) else null
-                    val property = Property(
-                            originName = name,
-                            originJsonValue = value,
-                            type = type.name,
-                            comment = schema.description ?: "",
-                            typeObject = type
-                    )
-                    properties.add(property)
-                }
-                else -> throw UnsupportedOperationException("Can't support this type in jsonschema ${jsonSchema.type}")
-            }
-        }
-        return DataClass(name = rootClassName, properties = properties)
+        val className = if (rootClassName.isNotBlank()) rootClassName else jsonSchema.title ?: ""
+        if (className.isBlank()) throw IllegalArgumentException("className cannot be null when jsonSchema.title is null")
+        return generateClass(jsonSchema, className)
     }
 
-    private fun getItemTypeOfArray(schema: JsonSchema): KotlinClass {
-        return if (schema.itemsOfArray?.isJsonObject == true) {
-            when (schema.itemsOfArray.asJsonObject["type"].asString) {
+    private fun generateClass(jsonObjectDef: JsonObjectDef, className: String): DataClass {
+        val description = jsonObjectDef.description
+        val properties = getProperties(jsonObjectDef)
+        return DataClass(
+                name = className,
+                properties = properties,
+                comments = description ?: "",
+                fromJsonSchema = true
+        )
+    }
+
+    private fun getProperties(jsonObjectDef: JsonObjectDef): List<Property> {
+        val properties = mutableListOf<Property>()
+        if (jsonObjectDef.properties != null) {
+            jsonObjectDef.properties.map { (propertyName, jsonProp) ->
+                resolveProperty(jsonProp, propertyName, jsonObjectDef.required?.contains(propertyName) ?: false)
+            }.toList().also { properties.addAll(it) }
+        }
+        if (jsonObjectDef.allOf != null) {
+            jsonObjectDef.allOf.flatMap { getProperties(it) }.toList().also { properties.addAll(it) }
+        }
+        if (jsonObjectDef.oneOf != null) {
+            jsonObjectDef.oneOf.flatMap { getProperties(it) }.toList().also { properties.addAll(it) }
+        }
+        if (jsonObjectDef.anyOf != null) {
+            jsonObjectDef.anyOf.flatMap { getProperties(it) }.toList().also { properties.addAll(it) }
+        }
+        if (jsonObjectDef.ref != null) {
+            getProperties(jsonSchema.resolveDefinition(jsonObjectDef.ref)).also { properties.addAll(it) }
+        }
+        if (jsonObjectDef.x_abstract == true) emptyList<Property>()
+        return properties
+    }
+
+    private fun resolveProperty(jsonProp: PropertyDef, propertyName: String, isRequired: Boolean): Property {
+        val typeClass = if (jsonProp.typeString == "array") {
+            val innerProperty = resolveProperty(jsonProp.items
+                    ?: throw IllegalArgumentException("Array `items` must be defined (property: $propertyName)"), propertyName, false)
+            GenericListClass(generic = innerProperty.typeObject)
+        } else {
+            val (jsonClassName, realDef) = getRealDefinition(jsonProp)
+            resolveTypeClass(realDef.typeString, jsonClassName, realDef, propertyName)
+        }
+//        val value = if (isRequired) getDefaultValue(typeClass.name) else null
+        val value = null
+        return Property(
+                originName = propertyName,
+                originJsonValue = value,
+                type = typeClass.name + if (jsonProp.isTypeNullable) "?" else "",
+                comment = jsonProp.description ?: "",
+                typeObject = typeClass,
+                nullable = jsonProp.isTypeNullable
+        )
+    }
+
+    private fun resolveTypeClass(realDefJsonType: String?, jsonClassName: String?, realDef: PropertyDef, propertyName: String, checkEnum: Boolean = true): KotlinClass {
+        val simpleName = jsonClassName ?: propertyName.capitalize()
+        return when {
+            checkEnum && realDef.enum != null -> resolveEnumClass(realDef, simpleName)
+            realDefJsonType != null && (jsonClassName != null || realDefJsonType == "object") -> generateClass(realDef, simpleName)
+            JSON_SCHEMA_FORMAT_MAPPINGS.containsKey(realDef.format) -> {
+                object : UnModifiableNoGenericClass() {
+                    override val name: String = JSON_SCHEMA_FORMAT_MAPPINGS[realDef.format]!!
+                }
+            }
+            else -> when (realDefJsonType) {
                 "string" -> KotlinClass.STRING
                 "enum" -> KotlinClass.STRING
                 "integer" -> KotlinClass.INT
@@ -112,7 +87,34 @@ class DataClassGeneratorByJSONSchema(private val rootClassName: String, private 
                 "boolean" -> KotlinClass.BOOLEAN
                 else -> KotlinClass.ANY
             }
-        } else KotlinClass.ANY
+        }
     }
 
+    private fun resolveEnumClass(enumDef: PropertyDef, name: String): KotlinClass {
+        if (enumDef.enum == null) throw IllegalArgumentException("$name is not a enum")
+        if (enumDef.x_enumNames != null && enumDef.enum.count() != enumDef.x_enumNames.count())
+            throw IllegalArgumentException("$name enum values count ${enumDef.enum.count()} not equal to enum names count ${enumDef.x_enumNames}")
+        val (jsonClassName, realDef) = getRealDefinition(enumDef)
+        val typeClass = resolveTypeClass(realDef.typeString, jsonClassName, realDef, name, !jsonClassName.isNullOrBlank())
+        return EnumClass(name = name, enum = enumDef.enum.asList(), xEnumNames = enumDef.x_enumNames?.asList(), generic = typeClass, comments = realDef.description
+                ?: "")
+    }
+
+    /** resolves `ref`, `oneOf` and `allOf` then returns a real property definition */
+    private fun getRealDefinition(def: PropertyDef): Pair<String? /* ClassName */, PropertyDef> = when {
+        (def.ref != null) -> Pair(def.tryGetClassName(), getRealDefinition(jsonSchema.resolveDefinition(def.ref)).second)
+        (def.oneOf != null) -> if (def.oneOf.count() > 1) getRealDefinition(def.oneOf.first { it.typeString != "null" })
+        else getRealDefinition(def.oneOf[0])
+        (def.allOf != null) -> {
+            val combinedProps: MutableMap<String, PropertyDef> = mutableMapOf()
+            def.allOf.forEach { p ->
+                val realDef = if (p.properties == null) getRealDefinition(p).second else p
+                if (realDef.properties != null)
+                    combinedProps.putAll(realDef.properties.map { pair -> pair.key to getRealDefinition(pair.value).second })
+            }
+            val combined = PropertyDef(type = "object", properties = combinedProps)
+            Pair(null, combined)
+        }
+        else -> Pair(null, def)
+    }
 }
