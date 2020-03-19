c4b215b08bf2f1baa2698ee72852906384661c11
theapache64 <theapache64@gmail.com>
2020-03-19 21:33:33 +0530

    ◀️ add hit database file + model class : backup

---
+++ api/src/test/kotlin/wu/seal/jsontokotlinclass/server/TestClass.kt
@@ -0,0 +1,10 @@
+package wu.seal.jsontokotlinclass.server
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.lang.StringBuilder
+
+class TestClass {
+    
+}
+
