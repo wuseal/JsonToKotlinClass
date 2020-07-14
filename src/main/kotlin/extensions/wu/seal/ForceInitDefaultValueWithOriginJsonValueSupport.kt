810c5e8c47bcedc3c6fc6cd62c81cfad5df5eb22
jose.han <jose.han@dingtone.me>
2020-07-14 20:21:18 +0800

    fix #301

--- src/main/kotlin/extensions/wu/seal/ForceInitDefaultValueWithOriginJsonValueSupport.kt
+++ src/main/kotlin/extensions/wu/seal/ForceInitDefaultValueWithOriginJsonValueSupport.kt
@@ -6,6 +6,7 @@ import wu.seal.jsontokotlin.model.classscodestruct.KotlinClass
 import wu.seal.jsontokotlin.model.codeelements.getDefaultValue
 import wu.seal.jsontokotlin.ui.jCheckBox
 import wu.seal.jsontokotlin.ui.jHorizontalLinearLayout
+import wu.seal.jsontokotlin.utils.StringEscapeUtils
 import wu.seal.jsontokotlin.utils.TYPE_STRING
 import javax.swing.JPanel
 
@@ -37,7 +38,7 @@ object ForceInitDefaultValueWithOriginJsonValueSupport : Extension() {
                 val newP = kotlinClass.properties.map {
                     val newV = if (it.originJsonValue.isNullOrBlank()) getDefaultValue(it.type) else {
                         if (it.type == TYPE_STRING) {
-                            """"${it.originJsonValue}""""
+                            """"${StringEscapeUtils.escapeJava(it.originJsonValue)}""""
                         } else {
                             it.originJsonValue
                         }
