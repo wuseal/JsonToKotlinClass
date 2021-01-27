8f153554f7d453b00a0eed1b70239f965d33d13b
theapache64 <theapache64@gmail.com>
2021-01-27 19:35:58 +0530

    💚 fix setAnnotationLibSerializable test

--- library/src/test/kotlin/wu/seal/jsontokotlin/JsonToKotlinBuilderTest.kt
+++ library/src/test/kotlin/wu/seal/jsontokotlin/JsonToKotlinBuilderTest.kt
@@ -444,14 +444,11 @@ class JsonToKotlinBuilderTest {
             
             import kotlinx.serialization.SerialName
             import kotlinx.serialization.Serializable
-            import kotlinx.serialization.Optional
             
             @Serializable
             data class User(
-                @Optional
                 @SerialName("name")
                 val name: String,
-                @Optional
                 @SerialName("company")
                 val company: String
             )
